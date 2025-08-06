82bf9cba9a10ef0ebe98cf557bd6e087f4c7a253
Ahmet Burak KIR <abkburak1@gmail.com>
2025-08-06 22:44:03 +0300

    Master

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 // lib/config/api_config.dart
 class ApiConfig {
-  static const String geminiApiKey = 'AIzaSyBiLvQfqGFQiK6o0awbyJnlTNX-HLS_fMk';
+  static const String geminiApiKey = 'YOUR_API_KEY_HERE';
   static const String aiModel = 'gemini-pro';
 }
